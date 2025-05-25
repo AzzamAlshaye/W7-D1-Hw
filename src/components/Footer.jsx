73ae3490f9b62644cef7d3e78529bfd779baa909
AzzamAlshaye <azzamwow@hotmail.com>
2025-05-25 16:14:53 +0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// export default function Footer() {
+//   return (
+//     <footer className="bg-gray-100 text-center p-4">
+//       &copy; {new Date().getFullYear()} Tuwaiq Academy
+//     </footer>
+//   );
+// }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+// import { Link } from "react-router";
+// import { IoMdMenu } from "react-icons/io";
+
+// export default function Nav() {
+//   return (
+//     <nav className="bg-black text-white p-6">
+//       <ul className="flex space-x-6 items-center lg:justify-normal justify-between">
+//         <img
+//           src="https://cdn.tuwaiq.edu.sa/landing/images/logo/logo-noname.png"
+//           className="h-10"
+//           alt=""
+//         />
+//         <IoMdMenu className="text-white text-3xl lg:hidden" />
+//         <div className="lg:flex hidden space-x-6 items-center">
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/courses">Courses</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About Us</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact Us</Link>
+//           </li>
+//         </div>
+//       </ul>
+//     </nav>
+//   );
+// }
